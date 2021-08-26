f5b2108748828cd364b94955b5e19d70a8e98600
Darshan Parmar <67963288+darshankparmar@users.noreply.github.com>
2021-08-26 12:24:43 +0530

    Update settings.py

--- Restaurant_management_system/settings.py
+++ Restaurant_management_system/settings.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'darshanvasaniya4841@gmail.com'
-EMAIL_HOST_PASSWORD = 'darshan123'
+EMAIL_HOST_USER = 'xxxx@gmail.com'   # enter you email address
+EMAIL_HOST_PASSWORD = 'xxxx'     # enter your password 
